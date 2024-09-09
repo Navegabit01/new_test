30e44a7cf627c6b5fadbcb0f8a58b2a61df699fc
LuisVidal <navegabit.2020@gmail.com>
2024-09-09 13:23:50 +0200

    Hotfix: Bug item views error response add

--- items/views.py
+++ items/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 from .models import Item
 from .serializers import ItemSerializer
 from .use_cases.item_use_cases import CreateItemUseCase
